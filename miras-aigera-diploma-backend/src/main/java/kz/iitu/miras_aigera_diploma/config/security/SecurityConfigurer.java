23b409593ea876e604ae886925a3cb12825beba2
Miras Madiyev <miras.madiev15@gmail.com>
2022-12-03 01:24:13 +0600

    cors fix

--- miras-aigera-diploma-backend/src/main/java/kz/iitu/miras_aigera_diploma/config/security/SecurityConfigurer.java
+++ miras-aigera-diploma-backend/src/main/java/kz/iitu/miras_aigera_diploma/config/security/SecurityConfigurer.java
@@ -14,8 +14,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(
@@ -67,20 +65,4 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
   public AuthenticationManager authenticationManagerBean() throws Exception {
     return super.authenticationManagerBean();
   }
-
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedHeaders("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
-            .allowedOriginPatterns("*")
-            .allowCredentials(true)
-            .maxAge(3600);
-      }
-    };
-  }
-
 }
