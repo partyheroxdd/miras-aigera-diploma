5bfece126f8f4f629931f36bef5d69707f85eaf2
Miras Madiyev <miras.madiev15@gmail.com>
2022-12-03 00:54:34 +0600

    cors fix

--- miras-aigera-diploma-backend/src/main/java/kz/iitu/miras_aigera_diploma/config/WebConfig.java
+++ miras-aigera-diploma-backend/src/main/java/kz/iitu/miras_aigera_diploma/config/WebConfig.java
@@ -1,12 +1,8 @@
 package kz.iitu.miras_aigera_diploma.config;
 
-import java.util.Arrays;
-import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,17 +11,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
   @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
-    final CorsConfiguration config = new CorsConfiguration();
-
-    config.setAllowedOrigins(List.of("http://localhost:3000"));
-    config.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
-    config.setAllowCredentials(true);
-    config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-
-    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", config);
-
-    return source;
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedHeaders("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
+            .allowedOriginPatterns("*")
+            .allowCredentials(true)
+            .maxAge(3600);
+      }
+    };
   }
+
 }
